d17af9c2ced153249f382f75040d96ab8690efc6
Jennifer Proctor <jen_p_13@hotmail.co.uk>
2018-10-19 13:58:12 +0100

    Refactored code, added CSS

--- js/app.js
+++ js/app.js
@@ -13,21 +13,48 @@ document.addEventListener('DOMContentLoaded', () => {
 const handleFormSubmit = function (event) {
   event.preventDefault();
 
-  const title = document.createElement("h2")
-  const genre = document.createElement("h3")
-  const rating = document.createElement("p")
-
-  title.textContent = `${this.title.value}`
-  genre.textContent = `${this.genre.value}`
-  rating.textContent = `${this.rating.value}`
-
+  const movieListItem = createMovieListItem(event.target);
   const result = document.querySelector("#movie-list");
-  result.appendChild(title);
-  result.appendChild(genre);
-  result.appendChild(rating);
+  result.appendChild(movieListItem);
+
+  // const title = document.createElement("h2")
+  // title.classList.add("movie-item");
+  // const genre = document.createElement("h3")
+  // genre.classList.add("movie-item");
+  // const rating = document.createElement("p")
+  // rating.classList.add("movie-item");
+
+  // title.textContent = `${this.title.value}`
+  // genre.textContent = `${this.genre.value}`
+  // rating.textContent = `${this.rating.value}`
+  //
+  // result.appendChild(title);
+  // result.appendChild(genre);
+  // result.appendChild(rating);
 
   document.getElementById('new-item-form').reset();
 };
+
+
+const createMovieListItem = function (form) {
+  const movieListItem = document.createElement("li");
+  movieListItem.classList.add("movie-list-item");
+
+  const title = document.createElement("h2");
+  title.textContent = form.title.value;
+  movieListItem.appendChild(title);
+
+  const genre = document.createElement("h3");
+  genre.textContent = form.genre.value;
+  movieListItem.appendChild(genre);
+
+  const rating = document.createElement("p");
+  rating.textContent = form.rating.value;
+  movieListItem.appendChild(rating);
+
+  return movieListItem
+};
+
 
 const handleDeleteAll = function () {
   document.getElementById("movie-list").textContent = "";
